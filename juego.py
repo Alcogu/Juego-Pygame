b613279f1f5c1a3f51f22a7d082305074523e951
Alexander Correa Gutiérrez <30854317+Alcogu@users.noreply.github.com>
2021-05-31 18:06:26 -0500

    se agregan pantalla inicial

---
+++ juego.py
@@ -0,0 +1,20 @@
+import pygame
+import sys
+
+azul = [76,160,233]
+rojo = [245,15,15]
+verde = [47, 163, 41]
+blanco = [255,255,255]
+negro = [0,0,0]
+
+ancho = 650
+alto = 350
+
+if __name__ == "__main__":
+    pygame.init()
+    pantalla=pygame.display.set_mode([ancho, alto])
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
