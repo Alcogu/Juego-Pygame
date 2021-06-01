84fda6dc8f133b77854f0936b8d06843b069d425
Alexander Correa Gutiérrez <30854317+Alcogu@users.noreply.github.com>
2021-06-01 13:51:39 -0500

    se agrega pj y movimiento en el fondo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from personaje import Personaje
 from matriz import recorte
 
 azul = [76,160,233]
@@ -8,12 +9,43 @@ verde = [47, 163, 41]
 blanco = [255,255,255]
 negro = [0,0,0]
 
-ancho = 900
+ancho = 800
 alto = 500
 
 if __name__ == "__main__":
     pygame.init()
     pantalla=pygame.display.set_mode([ancho, alto])
+    pygame.display.set_caption("Video Juego")
+    fuente = pygame.font.Font(None, 32)
+
+    fondo = pygame.image.load('fondo3.jpg')
+    info = fondo.get_rect()
+    f_ancho=info[2]
+    f_alto=info[3]
+    f_x = 0
+    f_vx = 0
+    f_y = 0
+    f_vy = 0
+    f_limx = ancho - f_ancho
+    f_limy = alto - f_alto
+    lim_d = 700
+    lim_a = 450
+    
+
+    personajes=pygame.sprite.Group()
+
+    sp_ancho=12
+    sp_alto=8
+    imgPersonaje = pygame.image.load('centauros.png')
+
+    m=recorte(sp_ancho, sp_alto, imgPersonaje)
+
+    desp=0
+    p=Personaje(m, [0, 2], desp)
+    personajes.add(p)
+    
+    col=0
+    reloj=pygame.time.Clock()
 
     while True:
         for event in pygame.event.get():
@@ -21,12 +53,55 @@ if __name__ == "__main__":
                 sys.exit()
 
             if event.type == pygame.KEYUP:
-                j.velx = 0
-                j.vely = 0
+                p.velx = 0
+                p.vely = 0
 
             if event.type == pygame.KEYDOWN:
-                #Salir con escape
+
                 if event.key == pygame.K_ESCAPE:
                     sys.exit()
-       
-        pantalla.fill(negro)
+                if event.key == pygame.K_UP:
+                    p.velx = 0
+                    p.vely = -5
+                    p.dir = desp + 3
+                if event.key == pygame.K_DOWN:
+                    p.velx = 0
+                    p.vely = 5
+                    p.dir = desp + 0
+                if event.key == pygame.K_RIGHT:
+                    p.velx = 5
+                    p.vely = 0
+                    p.dir = desp + 2
+                if event.key == pygame.K_LEFT:
+                    p.velx = -5
+                    p.vely = 0
+                    p.dir = desp + 1
+    
+        if p.rect.right > lim_d:
+            p.rect.right = lim_d
+            f_vx = -4
+        else:
+            f_vx = 0
+
+        if p.rect.bottom > lim_a:
+            p.rect.bottom = lim_a
+            f_vy = -4
+        else:
+            f_vy = 0
+            
+
+        personajes.update()
+        pantalla.blit(fondo,[f_x, f_y])
+        personajes.draw(pantalla)
+        pygame.display.flip()
+        reloj.tick(20)
+        if col < 2:
+            col+=1
+        else:
+            col=0
+
+        if f_x > f_limx:
+            f_x+=f_vx
+
+        if f_y > f_limy:
+            f_y+=f_vy
---
+++ personaje.py
@@ -0,0 +1,70 @@
+import pygame
+
+ancho = 900
+alto = 500
+
+class Personaje(pygame.sprite.Sprite):
+
+    def __init__(self, m, lim_anim=[0,2], desp=0):
+        pygame.sprite.Sprite.__init__(self)
+        self.m=m
+        self.anm_ini=lim_anim[0]
+        self.anm_fin=lim_anim[1]
+
+        self.col=self.anm_ini
+        self.dir=0+desp
+
+        self.image = self.m[self.dir][self.col]
+        self.rect = self.image.get_rect()
+
+        self.rect.x = 200
+        self.rect.y = 200
+        self.velx = 0
+        self.vely = 0
+        self.salud = 100
+        self.bloques = pygame.sprite.Group()
+
+    def update(self):
+        if self.velx != self.vely:
+            self.image = self.m[self.dir][self.col]
+
+            if self.col < self.anm_fin:
+                self.col+=1
+            else:
+                self.col=self.anm_ini
+
+            self.rect.x += self.velx
+
+        col = pygame.sprite.spritecollide(self, self.bloques, False)
+        for b in col: 
+            if self.velx > 0:
+                if self.rect.right > b.rect.left:
+                    self.rect.right = b.rect.left
+            else:
+                if self.rect.left < b.rect.right:
+                    self.rect.left = b.rect.right
+            self.velx=0
+
+        if self.rect.x < 0:
+            self.rect.left = 0
+
+        if self.rect.right > ancho:
+            self.rect.right = ancho
+
+        self.rect.y += self.vely
+
+        col = pygame.sprite.spritecollide(self, self.bloques, False)
+        for b in col: 
+            if self.vely > 0:
+                if self.rect.bottom > b.rect.top:
+                    self.rect.bottom = b.rect.top
+            else:
+                if self.rect.top < b.rect.bottom:
+                    self.rect.top = b.rect.bottom
+            self.vely=0
+
+        if self.rect.y < 0:
+            self.rect.y = 0
+
+        if self.rect.bottom > alto:
+            self.rect.bottom = alto
