080aa0fa11969eb850af9c386675fe2d641eddf4
Alexander Correa Gutiérrez <30854317+Alcogu@users.noreply.github.com>
2021-06-02 15:28:39 -0500

    por fin corregido error de desplazamiento

--- bloques.py
+++ bloques.py
@@ -21,7 +21,7 @@ class Bloques(pygame.sprite.Sprite):
         
         self.rect.x = pos[0]
         self.rect.y = pos[1]
-        self.velx = 0
 
     def update(self):
         self.rect.x += self.velx
+        self.rect.y += self.vely
