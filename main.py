1d30d34ae7f2e39d386162b533ad126e00efe28d
Alexander Correa Gutiérrez <30854317+Alcogu@users.noreply.github.com>
2021-06-04 22:54:40 -0500

    se comenta codigo en main, se agrega musica de fondo, contador de tiempo en partida y se agrega carpeta de sonidos

--- main.py
+++ main.py
@@ -16,35 +16,44 @@ alto = 420
 
 if __name__ == "__main__":
     pygame.init()
-    pantalla=pygame.display.set_mode([ancho, alto])
+    pantallaprincipal = pygame.display.set_mode([ancho, alto])
+    pantalla = pygame.Surface((ancho, alto-30))
     pygame.display.set_caption("Video Juego")
-    fuente = pygame.font.Font(None, 32)
+    fuente = pygame.font.Font('freesansbold.ttf', 25)
 
+    #Música de fondo
+    pygame.mixer.music.load('Juego-Pygame/sounds/mdf.wav')
+    #Primer valor para que la cancion no debe de sonar y segundo punto de inicio de la cancion
+    pygame.mixer.music.play(-1, 0.0)
+    #La canción va bajando volumen a medida que se va terminando
+    #pygame.mixer.music.fadeout(50000)
     
-    #Fondo = pygame.image.load('fondo3.jpg')
+    #grupos
     fondos=pygame.sprite.Group()
     personajes=pygame.sprite.Group()
     bloques = pygame.sprite.Group()
 
+    #Imagenes
     imgFondo = pygame.image.load('Juego-Pygame/imagenes/fondoprado2.jpg')
+    imgPersonaje = pygame.image.load('Juego-Pygame/imagenes/centauros.png')
+    imgBloque = pygame.image.load('Juego-Pygame/imagenes/bloques.png')
 
     f=Fondo(imgFondo)
     fondos.add(f)
     
-    imgPersonaje = pygame.image.load('Juego-Pygame/imagenes/centauros.png')
-
+    #Recorte imagen del PJ
     sp_ancho=12
     sp_alto=8
     m=recorte(sp_ancho, sp_alto, imgPersonaje)
+
+    #Eleccion del pj y sus movimientos
     desp=0
     p=Personaje(m, [0, 2], desp)
     personajes.add(p)
 
-    
+    #Recorte de la imagen para los bloques
     bl_ancho=2
     bl_alto=12
-    imgBloque = pygame.image.load('Juego-Pygame/imagenes/bloques.png')
-
     mb=recorte(bl_ancho, bl_alto, imgBloque)
 
     b=Bloques([120, 120], mb, [0, 0], despb=0)
@@ -117,6 +126,7 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            #PJ sin movimiento
             if event.type == pygame.KEYUP:
                 p.velx = 0
                 p.vely = 0
@@ -142,12 +152,14 @@ if __name__ == "__main__":
                     p.vely = 0
                     p.dir = desp + 1
     
+        #Movimiento en mapa hacia la derecha
         if p.rect.right > f.lim_d:
             p.rect.right = f.lim_d
             f.f_vx = -5
         else:
             f.f_vx = 0
 
+        #Movimiento en mapa hacia abajo
         if p.rect.bottom > f.lim_a:
             p.rect.bottom = f.lim_a
             f.f_vy = -5
@@ -158,18 +170,30 @@ if __name__ == "__main__":
             b.velx = f.f_vx   
             b.vely = f.f_vy
             
+        #Actualizaciones
         personajes.update()
         bloques.update()
 
+        #Tiempo transcurrido de la partida
+        tiempo = pygame.time.get_ticks() // 1000
+        info = "Tiempo: " + str(tiempo)
+        texto = fuente.render(info, False, blanco)
+
+        #Se dibuja en pantalla
+        pantallaprincipal.fill(negro)
+        pantallaprincipal.blit(pantalla, (0,30))
         pantalla.blit(imgFondo,[f.f_x, f.f_y])
+        pantallaprincipal.blit(texto, [2, 2])
         personajes.draw(pantalla)
         bloques.draw(pantalla)
 
         pygame.display.flip()
         reloj.tick(70)
 
+        #Desplazamiento de la pantalla en la imagen de fondo a la derecha
         if f.f_x > f.f_limx:
             f.f_x+=f.f_vx
 
+        #Desplazamiento de la pantalla en la imagen de fondo a abajo
         if f.f_y > f.f_limy:
             f.f_y+=f.f_vy
